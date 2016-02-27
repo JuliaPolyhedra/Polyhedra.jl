f16f7d893b3191f1c701e7af657991abbeb0c967
Benoît Legat <benoit.legat@gmail.com>
2016-02-27 18:48:34 +0100

    Fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,12 +4,12 @@ using Base.Test
 A = [1 1; -1 0; 0 -1]
 linset = IntSet([1])
 
-@test_throws ErrorException Polyhedra.InequalityDescription(A, [0, 0], linset)
-@test_throws ErrorException Polyhedra.InequalityDescription(A, [0, 0], IntSet([4]))
+@test_throws ErrorException InequalityDescription(A, [0, 0], linset)
+@test_throws ErrorException InequalityDescription(A, [0, 0], IntSet([4]))
 
 V = [0 1; 1 0]
-@test_throws ErrorException Polyhedra.GeneratorDescription(V, [1 0 0], vertex, IntSet([]), IntSet([]))
-@test_throws ErrorException Polyhedra.GeneratorDescription(V, [1 1], vertex, IntSet([]), IntSet([2]))
-@test_throws ErrorException Polyhedra.GeneratorDescription(V, [1 1], vertex, IntSet([4]), IntSet([]))
-@test_throws ErrorException Polyhedra.GeneratorDescription(V, IntSet([4]))
-
+vertex = IntSet([1, 2])
+@test_throws ErrorException GeneratorDescription(V, [1 0 0], vertex, IntSet([]), IntSet([]))
+@test_throws ErrorException GeneratorDescription(V, [1 1], vertex, IntSet([]), IntSet([2]))
+@test_throws ErrorException GeneratorDescription(V, [1 1], vertex, IntSet([4]), IntSet([]))
+@test_throws ErrorException GeneratorDescription(V, IntSet([4]))
