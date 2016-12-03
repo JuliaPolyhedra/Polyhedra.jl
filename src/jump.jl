f8732c8c62e0160c3fdce5a2362dccb29d99e0e8
Benoît Legat <benoit.legat@gmail.com>
2016-12-03 14:18:35 +0100

    Forgot to commit jump.jl

---
+++ src/jump.jl
@@ -0,0 +1,20 @@
+using JuMP
+
+function LPHRepresentation(model::JuMP.Model)
+    # Inspired from Joey Huchette's code in ConvexHull.jl
+    A = JuMP.prepConstrMatrix(model)
+    c, lb, ub = JuMP.prepProblemBounds(model)
+    l, u = model.colLower, model.colUpper
+
+    m, n = size(A)
+    @assert m == length(lb) == length(ub)
+    @assert model.nlpdata == nothing
+    @assert isempty(model.quadconstr)
+    @assert isempty(model.sosconstr)
+
+    LPHRepresentation(A, l, u, lb, ub)
+end
+
+function polyhedron(model::JuMP.Model, lib::PolyhedraLibrary=getlibraryfor(Float64))
+    polyhedron(LPHRepresentation(model), lib)
+end
