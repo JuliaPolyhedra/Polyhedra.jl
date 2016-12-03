e5c5303976902d91024118287e3cb12fdb6f25fc
Benoît Legat <benoit.legat@gmail.com>
2016-12-03 18:33:35 +0100

    Decrease Test printing in case of success

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,11 +8,13 @@ include("alltests.jl")
 include("libraries.jl")
 
 for (testname, testfun) in alltests
-    for arith in [("floating point", float_libraries), ("exact", exact_libraries)]
-        arithname, arith_libraries = arith
-        @testset "$testname tests in $arithname arithmetic with $(typeof(library))" for library in arith_libraries
-            testfun(library)
+    @testset "$testname tests" begin
+        for arith in [("floating point", float_libraries), ("exact", exact_libraries)]
+            arithname, arith_libraries = arith
+            @testset "$testname tests in $arithname arithmetic with $(typeof(library))" for library in arith_libraries
+                testfun(library)
+            end
+            length(arith_libraries) == 0 && warn("$testname tests in $arithname arithmetics test not run!")
         end
-        length(arith_libraries) == 0 && warn("$testname tests in $arithname arithmetics test not run!")
     end
 end
