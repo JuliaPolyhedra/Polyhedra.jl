16e70060152ce06d3a5bbef9803ec13f762c8d14
autodocs <autodocs>
2017-10-24 13:59:38 +0000

    build based on ddd991c

--- latest/search_index.js
+++ latest/search_index.js
@@ -29,7 +29,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Index",
     "title": "Contents",
     "category": "section",
-    "text": "Pages = [\"installation.md\", \"representation.md\", \"polyhedron.md\"]\nDepth = 2"
+    "text": "Pages = [\"installation.md\", \"representation.md\", \"polyhedron.md\", \"utilities.md\"]\nDepth = 2"
 },
 
 {
@@ -89,6 +89,110 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "representation.html#Polyhedra.fulldim",
+    "page": "Representation",
+    "title": "Polyhedra.fulldim",
+    "category": "Function",
+    "text": "fulldim(rep::Rep)\n\nReturns the dimension of the space in which the representation is defined. That is, a straight line in a 3D space has fulldim 3.\n\n\n\n"
+},
+
+{
+    "location": "representation.html#Representation-interface-1",
+    "page": "Representation",
+    "title": "Representation interface",
+    "category": "section",
+    "text": "These functions can be called on both H-representation and V-representationfulldim"
+},
+
+{
+    "location": "representation.html#Polyhedra.hreps",
+    "page": "Representation",
+    "title": "Polyhedra.hreps",
+    "category": "Function",
+    "text": "hreps(hr::HRep)\n\nReturns an iterator over the elements of the H-representation.\n\nNote\n\nThis is type unstable as the iterator returns both halfspaces and hyperplanes. It is therefore more efficient to call eqs and ineqs separately.\n\n\n\n"
+},
+
+{
+    "location": "representation.html#Polyhedra.nhreps",
+    "page": "Representation",
+    "title": "Polyhedra.nhreps",
+    "category": "Function",
+    "text": "nhreps(hr::HRep)\n\nReturns the number of halfspaces and hyperplanes of the H-representation.\n\nNote\n\nNote that it does not do redundancy removal so it is not the minimal number of halfspace and hyperplanes needed to represent the polyhedron, it is simply the number that are currently used.\n\n\n\n"
+},
+
+{
+    "location": "representation.html#Polyhedra.hashreps",
+    "page": "Representation",
+    "title": "Polyhedra.hashreps",
+    "category": "Function",
+    "text": "hashreps(hr::HRep)\n\nReturns whether the H-representation contain any halfspace or hyperplane.\n\n\n\n"
+},
+
+{
+    "location": "representation.html#Polyhedra.eqs",
+    "page": "Representation",
+    "title": "Polyhedra.eqs",
+    "category": "Function",
+    "text": "eqs(hr::HRep)\n\nReturns an iterator over the hyperplanes of the H-representation.\n\n\n\n"
+},
+
+{
+    "location": "representation.html#Polyhedra.neqs",
+    "page": "Representation",
+    "title": "Polyhedra.neqs",
+    "category": "Function",
+    "text": "neqs(hr::HRep)\n\nReturns the number of hyperplanes of the H-representation.\n\n\n\n"
+},
+
+{
+    "location": "representation.html#Polyhedra.haseqs",
+    "page": "Representation",
+    "title": "Polyhedra.haseqs",
+    "category": "Function",
+    "text": "haseqs(hr::HRep)\n\nReturns whether the H-representation contain any hyperplane.\n\n\n\n"
+},
+
+{
+    "location": "representation.html#Polyhedra.ineqs",
+    "page": "Representation",
+    "title": "Polyhedra.ineqs",
+    "category": "Function",
+    "text": "ineqs(hr::HRep)\n\nReturns an iterator over the halfspaces of the H-representation.\n\n\n\n"
+},
+
+{
+    "location": "representation.html#Polyhedra.nineqs",
+    "page": "Representation",
+    "title": "Polyhedra.nineqs",
+    "category": "Function",
+    "text": "nineqs(hr::HRep)\n\nReturns the number of halfspaces of the H-representation.\n\n\n\n"
+},
+
+{
+    "location": "representation.html#Polyhedra.hasineqs",
+    "page": "Representation",
+    "title": "Polyhedra.hasineqs",
+    "category": "Function",
+    "text": "hasineqs(hr::HRep)\n\nReturns whether the H-representation contain any halfspace.\n\n\n\n"
+},
+
+{
+    "location": "representation.html#H-representation-interface-1",
+    "page": "Representation",
+    "title": "H-representation interface",
+    "category": "section",
+    "text": "hreps\nnhreps\nhashreps\neqs\nneqs\nhaseqs\nineqs\nnineqs\nhasineqs"
+},
+
+{
+    "location": "representation.html#V-representation-interface-1",
+    "page": "Representation",
+    "title": "V-representation interface",
+    "category": "section",
+    "text": ""
+},
+
+{
     "location": "polyhedron.html#",
     "page": "Polyhedron",
     "title": "Polyhedron",
@@ -126,6 +230,118 @@ var documenterSearchIndex = {"docs": [
     "title": "Projecting a polyhedron",
     "category": "section",
     "text": "Consider the polyhedron created in the beginning of this section. As a reminder, it represents the following H-representation:beginalign*\n  x_1 + x_2 leq 1 \n  x_1 - x_2 leq 0 \n  x_1  geq 0\nendalign*One can verify that for any 0 leq x_2 leq 1, there exists a value x_1 such that (x_1 x_2) is in this polyhedron. This means that the H-representation obtained by eliminating x_1 is:beginalign*\n  x_1  leq 1 \n  x_1  geq 0\nendalign*where x_1 in the H-representation above represents x_2 in the previous one. This can be obtained as followsjulia> poly_x2 = eliminate(poly, [1])\njulia> hrep(poly_x2)\nH-representation\nbegin\n 2 2 rational\n 1//1 -1//1\n 0//1 1//1\nendThere is two methods of computing the elimination implemented in CDDLib: Fourier-Motzkin elimination and block elimination. As written by K. Fukuda in CDD's documentation, \"[Block elimination] might be a faster way to eliminate variables than the repeated [Fourier-Motzkin elimination] when the number of variables to eliminate is large\". You can specify the method to use as a third argument, e.g. eliminate(poly, [1], :FourierMotzkin), eliminate(poly, [1], :BlockElimination). A third method can be chosen: :ProjectGenerators. It computes the V-representation and then project each of its elements. This is the method of choice when the V-representation is already computed.If nothing is specified as in the block of code above, the behavior depends on the polyhedral library. If neither Fourier-Motzkin nor block elimination is implemented or if the V-representation is already computed then :ProjectGenerators is chosen. Otherwise, Polyhedra lets the library decide. In CDDLib, :FourierMotzkin is chosen when only the last dimension needs to be eliminated and :BlockElimination is chosen otherwise. Note that CDDLib only supports projecting the last trailing dimensions."
+},
+
+{
+    "location": "utilities.html#",
+    "page": "Utilities",
+    "title": "Utilities",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "utilities.html#Utilities-1",
+    "page": "Utilities",
+    "title": "Utilities",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "utilities.html#Polyhedra.convexhull",
+    "page": "Utilities",
+    "title": "Polyhedra.convexhull",
+    "category": "Function",
+    "text": "convexhull(P1::VRep, P2::VRep)\n\nTakes the convex hull of P1 and P2  lambda x + (1-lambda) y  x in P1 y in P2 . It is very efficient between two V-representations or between two polyhedron for which the V-representation has already been computed. However, if P1 (resp. P2) is a polyhedron for which the V-representation has not been computed yet, it will trigger a representation conversion which is costly.\n\nThe type of the result will be chosen closer to the type of P1. For instance, if P1 is a polyhedron (resp. V-representation) and P2 is a V-representation (resp. polyhedron), convexhull(P1, P2) will be a polyhedron (resp. V-representation). If P1 and P2 are both polyhedra (resp. V-representation), the resulting polyhedron type (V-representation type) will be computing according to the type of P1. The coefficient type however, will be promoted as required taking both the coefficient type of P1 and P2 into account. ```\n\n\n\n"
+},
+
+{
+    "location": "utilities.html#Operations-1",
+    "page": "Utilities",
+    "title": "Operations",
+    "category": "section",
+    "text": "convexhull"
+},
+
+{
+    "location": "utilities.html#Polyhedra.ininterior",
+    "page": "Utilities",
+    "title": "Polyhedra.ininterior",
+    "category": "Function",
+    "text": "ininterior(p::VRepElement, h::HRepElement)\n\nReturns whether p is in the interior of h. If h is an hyperplane, it always returns false. If h is an halfspace langle a x rangle leq beta, it returns whether p is in the open halfspace langle a x rangle  beta\n\nininterior(p::VRepElement, h::HRep)\n\nReturns whether p is in the interior of h, e.g. in the interior of all the hyperplanes and halfspaces supporting h.\n\n\n\n"
+},
+
+{
+    "location": "utilities.html#Polyhedra.inrelativeinterior",
+    "page": "Utilities",
+    "title": "Polyhedra.inrelativeinterior",
+    "category": "Function",
+    "text": "inrelativeinterior(p::VRepElement, h::HRepElement)\n\nReturns whether p is in the relative interior of h. If h is an hyperplane, it is equivalent to p in h since the relative interior of an hyperplane is itself. If h is an halfspace, it is equivalent to ininterior(p, h).\n\ninrelativeinterior(p::VRepElement, h::HRep)\n\nReturns whether p is in the relative interior of h, e.g. in the relative interior of all the hyperplanes and halfspaces supporting h.\n\n\n\n"
+},
+
+{
+    "location": "utilities.html#Containment-1",
+    "page": "Utilities",
+    "title": "Containment",
+    "category": "section",
+    "text": "ininterior\ninrelativeinterior"
+},
+
+{
+    "location": "utilities.html#Polyhedra.volume",
+    "page": "Utilities",
+    "title": "Polyhedra.volume",
+    "category": "Function",
+    "text": "volume(p::Polyhedron{N, T}) where {N, T}\n\nReturns the N-dimensional hyper-volume of the polyhedron p. Returns Inf or -one(T) if it is infinite depending on whether the type T has an infinite value.\n\n\n\n"
+},
+
+{
+    "location": "utilities.html#Polyhedra.surface",
+    "page": "Utilities",
+    "title": "Polyhedra.surface",
+    "category": "Function",
+    "text": "surface(p::Polyhedron{N, T}) where {N, T}\n\nReturns the N-1-dimensional hyper-volume of the surface of the polyhedron p. Returns Inf or -one(T) if it is infinite depending on whether the type T has an infinite value.\n\n\n\n"
+},
+
+{
+    "location": "utilities.html#Volume-1",
+    "page": "Utilities",
+    "title": "Volume",
+    "category": "section",
+    "text": "volume\nsurface"
+},
+
+{
+    "location": "utilities.html#Polyhedra.chebyshevcenter",
+    "page": "Utilities",
+    "title": "Polyhedra.chebyshevcenter",
+    "category": "Function",
+    "text": "chebyshevcenter(p::Rep[, solver])\n\nIf p is a H-representation or is a polyhedron for which the H-representation has already been computed, calls hchebyshevcenter, otherwise, call vchebyshevcenter.\n\n\n\n"
+},
+
+{
+    "location": "utilities.html#Polyhedra.hchebyshevcenter",
+    "page": "Utilities",
+    "title": "Polyhedra.hchebyshevcenter",
+    "category": "Function",
+    "text": "hchebyshevcenter(p::HRep[, solver])\n\nReturn a tuple with the center and radius of the largest euclidean ball contained in the polyhedron p. Throws an error if the polyhedron is empty or if the radius is infinite.\n\n\n\n"
+},
+
+{
+    "location": "utilities.html#Polyhedra.vchebyshevcenter",
+    "page": "Utilities",
+    "title": "Polyhedra.vchebyshevcenter",
+    "category": "Function",
+    "text": "vchebyshevcenter(p::VRep[, solver])\n\nReturn a tuple with the center and radius of the smallest euclidean ball containing the polyhedron p. Throws an error if the polyhedron is empty or if the radius is infinite (i.e. p is not a polytope, it contains rays).\n\n\n\n"
+},
+
+{
+    "location": "utilities.html#Chebyshev-center-1",
+    "page": "Utilities",
+    "title": "Chebyshev center",
+    "category": "section",
+    "text": "chebyshevcenter\nhchebyshevcenter\nvchebyshevcenter"
 },
 
 ]}
