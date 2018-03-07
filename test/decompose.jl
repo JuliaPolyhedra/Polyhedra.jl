6188374865855639bc40d6104d8fedbd97e4019e
Benoît Legat <benoit.legat@gmail.com>
2018-03-08 00:22:26 +0100

    Improve decompose tests

--- test/decompose.jl
+++ test/decompose.jl
@@ -1,3 +1,72 @@
+struct Face
+    points::Vector{Vector{Float64}}
+end
+Face(args...) = Face([args...])
+
+function Base.isapprox(a::Face, b::Face; atol=1e-8, rtol=1e-8)
+    for i in 1:3
+        ok = false
+        for j in 1:3
+            if !isapprox(a.points[j], b.points[((j+i-1)%3)+1]; atol=atol, rtol=rtol)
+                ok = true
+            end
+        end
+        ok && return true
+    end
+    return false
+end
+
+nfaces(d::Dict{<:Any, Face}) = length(d)
+nfaces(d::Dict{<:Any, <:Vector}) = sum(map(length, values(d)))
+function test_decompose(p::Polyhedron, d::Dict)
+    points = GeometryTypes.decompose(GeometryTypes.Point{3, Float32}, p)
+    faces = GeometryTypes.decompose(GeometryTypes.Face{3, Int}, p)
+    normals = GeometryTypes.decompose(GeometryTypes.Normal{3,Float64}, p)
+    nf = nfaces(d)
+    @test length(points) == 3nf
+    @test length(faces) == nf
+    @test length(normals) == 3nf
+    for i in eachindex(faces)
+        a, b, c = faces[i]
+        @test normals[3i] == normals[3i-1] == normals[3i-2]
+        normal = map(x -> x === -0.0 ? 0.0 : x, round.(normals[3i], 6))
+        face = Face(points[3i-2],
+                    points[3i-1],
+                    points[3i])
+        @test normal in keys(d)
+        f = d[normal]
+        if f isa Face
+            @test f ≈ face
+            delete!(d, normal)
+        else
+            j = findfirst(fa -> fa ≈ face, f)
+            @test j isa Int && j > 0
+            deleteat!(f, j)
+            if isempty(f)
+                delete!(d, normal)
+            end
+        end
+    end
+    @test isempty(d)
+end
+
+function orthantdecompose(lib::PolyhedraLibrary)
+    v = vrep([Ray([1., 0, 0]),
+              Ray([0, 1., 0]),
+              Ray([0, 0, 1.])])
+    p = polyhedron(v, lib)
+    d = Dict([-1.0,  0.0,  0.0] => Face([0.0, 0.0, 0.0],
+                                        [0.0, 2.0, 0.0],
+                                        [0.0, 0.0, 2.0]),
+             [ 0.0, -1.0,  0.0] => Face([0.0, 0.0, 0.0],
+                                        [0.0, 0.0, 2.0],
+                                        [2.0, 0.0, 0.0]),
+             [ 0.0,  0.0, -1.0] => Face([0.0, 0.0, 0.0],
+                                        [2.0, 0.0, 0.0],
+                                        [0.0, 2.0, 0.0]))
+    test_decompose(p, d)
+end
+
 function cubedecompose(lib::PolyhedraLibrary)
     p = polyhedron(convexhull([ 1.,  1,   1],
                               [ 1., -1,   1],
@@ -6,24 +75,27 @@ function cubedecompose(lib::PolyhedraLibrary)
                               [-1.,  1,  1],
                               [-1., -1,  1],
                               [-1.,  1, -1],
-                              [-1., -1, -1]))
+                              [-1., -1, -1]), lib)
     # FIXME failing
     #p = polyhedron(convexhull(SymPoint([1., 1, 1]),
     #                          SymPoint([1., -1, 1]),
     #                          SymPoint([1., 1, -1]),
     #                          SymPoint([1., -1, -1])))
-    points = GeometryTypes.decompose(GeometryTypes.Point{3, Float32}, p)
-    faces = GeometryTypes.decompose(GeometryTypes.Face{3, Int}, p)
-    normals = GeometryTypes.decompose(GeometryTypes.Normal{3,Float64}, p)
-    @test length(points) == 36
-    @test length(faces) == 12
-    @test length(normals) == 36
-#    Dict([0, 0, -1] => [[[-1.0, -1.0, -1.0], [1.0, -1.0, -1.0], [1.0, 1.0, -1.0]],
-#                        [[1.0, 1.0, -1.0], [-1.0, 1.0, -1.0], [-1.0, -1.0, -1.0]]
+    d = Dict([ 0.0,  0.0, -1.0] => [Face([-1.0, -1.0, -1.0], [ 1.0, -1.0, -1.0], [ 1.0,  1.0, -1.0]),
+                                    Face([ 1.0,  1.0, -1.0], [-1.0,  1.0, -1.0], [-1.0, -1.0, -1.0])],
+             [-1.0,  0.0,  0.0] => [Face([-1.0, -1.0, -1.0], [-1.0,  1.0, -1.0], [-1.0,  1.0,  1.0]),
+                                    Face([-1.0,  1.0,  1.0], [-1.0, -1.0,  1.0], [-1.0, -1.0, -1.0])],
+             [ 0.0, -1.0,  0.0] => [Face([-1.0, -1.0,  1.0], [ 1.0, -1.0,  1.0], [ 1.0, -1.0, -1.0]),
+                                    Face([ 1.0, -1.0, -1.0], [-1.0, -1.0, -1.0], [-1.0, -1.0,  1.0])],
+             [ 0.0,  1.0,  0.0] => [Face([-1.0,  1.0, -1.0], [ 1.0,  1.0, -1.0], [ 1.0,  1.0,  1.0]),
+                                    Face([ 1.0,  1.0,  1.0], [-1.0,  1.0,  1.0], [-1.0,  1.0, -1.0])],
+             [ 0.0,  0.0,  1.0] => [Face([-1.0,  1.0,  1.0], [ 1.0,  1.0,  1.0], [ 1.0, -1.0,  1.0]),
+                                    Face([ 1.0, -1.0,  1.0], [-1.0, -1.0,  1.0], [-1.0,  1.0,  1.0])],
+             [ 1.0,  0.0,  0.0] => [Face([ 1.0, -1.0,  1.0], [ 1.0,  1.0,  1.0], [ 1.0,  1.0, -1.0]),
+                                    Face([ 1.0,  1.0, -1.0], [ 1.0, -1.0, -1.0], [ 1.0, -1.0,  1.0])])
+    test_decompose(p, d)
 end
 function largedecompose(lib::PolyhedraLibrary)
-    isa(lib, SimplePolyhedraLibrary) && return
-    contains(string(typeof(lib)),"LRSLibrary") && return
     V = [-1 -1  1;
          -1  1  1;
           1 -1  1;
@@ -35,141 +107,33 @@ function largedecompose(lib::PolyhedraLibrary)
     R = [0 0 1]
     v = vrep(V, R)
     p = polyhedron(v, lib)
-    @test GeometryTypes.decompose(GeometryTypes.Point{3, Float32}, p) ==
-    [GeometryTypes.Point(1.0,1.0,1.0),
-     GeometryTypes.Point(1.0,-1.0,1.0),
-     GeometryTypes.Point(1.0,1.0,2.0),
-     GeometryTypes.Point(1.0,-1.0,1.0),
-     GeometryTypes.Point(1.0,-1.0,2.0),
-     GeometryTypes.Point(1.0,1.0,2.0),
-     GeometryTypes.Point(1.0,1.0,1.0),
-     GeometryTypes.Point(1.0,0.0,-1.0),
-     GeometryTypes.Point(1.0,-1.0,1.0),
-     GeometryTypes.Point(-1.0,1.0,1.0),
-     GeometryTypes.Point(1.0,1.0,1.0),
-     GeometryTypes.Point(-1.0,1.0,2.0),
-     GeometryTypes.Point(1.0,1.0,1.0),
-     GeometryTypes.Point(1.0,1.0,2.0),
-     GeometryTypes.Point(-1.0,1.0,2.0),
-     GeometryTypes.Point(-1.0,1.0,1.0),
-     GeometryTypes.Point(0.0,1.0,-1.0),
-     GeometryTypes.Point(1.0,1.0,1.0),
-     GeometryTypes.Point(1.0,1.0,1.0),
-     GeometryTypes.Point(0.0,1.0,-1.0),
-     GeometryTypes.Point(1.0,0.0,-1.0),
-     GeometryTypes.Point(1.0,-1.0,1.0),
-     GeometryTypes.Point(-1.0,-1.0,1.0),
-     GeometryTypes.Point(1.0,-1.0,2.0),
-     GeometryTypes.Point(-1.0,-1.0,1.0),
-     GeometryTypes.Point(-1.0,-1.0,2.0),
-     GeometryTypes.Point(1.0,-1.0,2.0),
-     GeometryTypes.Point(1.0,-1.0,1.0),
-     GeometryTypes.Point(0.0,-1.0,-1.0),
-     GeometryTypes.Point(-1.0,-1.0,1.0),
-     GeometryTypes.Point(-1.0,-1.0,1.0),
-     GeometryTypes.Point(-1.0,1.0,1.0),
-     GeometryTypes.Point(-1.0,-1.0,2.0),
-     GeometryTypes.Point(-1.0,1.0,1.0),
-     GeometryTypes.Point(-1.0,1.0,2.0),
-     GeometryTypes.Point(-1.0,-1.0,2.0),
-     GeometryTypes.Point(-1.0,-1.0,1.0),
-     GeometryTypes.Point(-1.0,0.0,-1.0),
-     GeometryTypes.Point(-1.0,1.0,1.0),
-     GeometryTypes.Point(-1.0,-1.0,1.0),
-     GeometryTypes.Point(0.0,-1.0,-1.0),
-     GeometryTypes.Point(-1.0,0.0,-1.0),
-     GeometryTypes.Point(-1.0,0.0,-1.0),
-     GeometryTypes.Point(0.0,1.0,-1.0),
-     GeometryTypes.Point(-1.0,1.0,1.0),
-     GeometryTypes.Point(1.0,0.0,-1.0),
-     GeometryTypes.Point(0.0,-1.0,-1.0),
-     GeometryTypes.Point(1.0,-1.0,1.0),
-     GeometryTypes.Point(0.0,-1.0,-1.0),
-     GeometryTypes.Point(1.0,0.0,-1.0),
-     GeometryTypes.Point(0.0,1.0,-1.0),
-     GeometryTypes.Point(0.0,1.0,-1.0),
-     GeometryTypes.Point(-1.0,0.0,-1.0),
-     GeometryTypes.Point(0.0,-1.0,-1.0)]
-
-     # TODO: restore this test so that it tries to decompose into a Face with
-     # a non-zero offset, once https://github.com/JuliaGeometry/GeometryTypes.jl/issues/89 is resolved
-    @test GeometryTypes.decompose(GeometryTypes.Face{3, Int}, p) ==
-    [GeometryTypes.Face{3, Int}(2,3,4) .- 1,
-     GeometryTypes.Face{3, Int}(5,6,7) .- 1,
-     GeometryTypes.Face{3, Int}(8,9,10) .- 1,
-     GeometryTypes.Face{3, Int}(11,12,13) .- 1,
-     GeometryTypes.Face{3, Int}(14,15,16) .- 1,
-     GeometryTypes.Face{3, Int}(17,18,19) .- 1,
-     GeometryTypes.Face{3, Int}(20,21,22) .- 1,
-     GeometryTypes.Face{3, Int}(23,24,25) .- 1,
-     GeometryTypes.Face{3, Int}(26,27,28) .- 1,
-     GeometryTypes.Face{3, Int}(29,30,31) .- 1,
-     GeometryTypes.Face{3, Int}(32,33,34) .- 1,
-     GeometryTypes.Face{3, Int}(35,36,37) .- 1,
-     GeometryTypes.Face{3, Int}(38,39,40) .- 1,
-     GeometryTypes.Face{3, Int}(41,42,43) .- 1,
-     GeometryTypes.Face{3, Int}(44,45,46) .- 1,
-     GeometryTypes.Face{3, Int}(47,48,49) .- 1,
-     GeometryTypes.Face{3, Int}(50,51,52) .- 1,
-     GeometryTypes.Face{3, Int}(53,54,55) .- 1]
-    @test GeometryTypes.decompose(GeometryTypes.Normal{3,Float64}, p) ==
-    [GeometryTypes.Normal(1.0,0.0,0.0),
-     GeometryTypes.Normal(1.0,0.0,0.0),
-     GeometryTypes.Normal(1.0,0.0,0.0),
-     GeometryTypes.Normal(1.0,0.0,0.0),
-     GeometryTypes.Normal(1.0,0.0,0.0),
-     GeometryTypes.Normal(1.0,0.0,0.0),
-     GeometryTypes.Normal(1.0,0.0,0.0),
-     GeometryTypes.Normal(1.0,0.0,0.0),
-     GeometryTypes.Normal(1.0,0.0,0.0),
-     GeometryTypes.Normal(0.0,1.0,0.0),
-     GeometryTypes.Normal(0.0,1.0,0.0),
-     GeometryTypes.Normal(0.0,1.0,0.0),
-     GeometryTypes.Normal(0.0,1.0,0.0),
-     GeometryTypes.Normal(0.0,1.0,0.0),
-     GeometryTypes.Normal(0.0,1.0,0.0),
-     GeometryTypes.Normal(0.0,1.0,0.0),
-     GeometryTypes.Normal(0.0,1.0,0.0),
-     GeometryTypes.Normal(0.0,1.0,0.0),
-     GeometryTypes.Normal(2/3,2/3,-1/3),
-     GeometryTypes.Normal(2/3,2/3,-1/3),
-     GeometryTypes.Normal(2/3,2/3,-1/3),
-     GeometryTypes.Normal(0.0,-1.0,0.0),
-     GeometryTypes.Normal(0.0,-1.0,0.0),
-     GeometryTypes.Normal(0.0,-1.0,0.0),
-     GeometryTypes.Normal(0.0,-1.0,0.0),
-     GeometryTypes.Normal(0.0,-1.0,0.0),
-     GeometryTypes.Normal(0.0,-1.0,0.0),
-     GeometryTypes.Normal(0.0,-1.0,0.0),
-     GeometryTypes.Normal(0.0,-1.0,0.0),
-     GeometryTypes.Normal(0.0,-1.0,0.0),
-     GeometryTypes.Normal(-1.0,0.0,0.0),
-     GeometryTypes.Normal(-1.0,0.0,0.0),
-     GeometryTypes.Normal(-1.0,0.0,0.0),
-     GeometryTypes.Normal(-1.0,0.0,0.0),
-     GeometryTypes.Normal(-1.0,0.0,0.0),
-     GeometryTypes.Normal(-1.0,0.0,0.0),
-     GeometryTypes.Normal(-1.0,0.0,0.0),
-     GeometryTypes.Normal(-1.0,0.0,0.0),
-     GeometryTypes.Normal(-1.0,0.0,0.0),
-     GeometryTypes.Normal(-2/3,-2/3,-1/3),
-     GeometryTypes.Normal(-2/3,-2/3,-1/3),
-     GeometryTypes.Normal(-2/3,-2/3,-1/3),
-     GeometryTypes.Normal(-2/3,2/3,-1/3),
-     GeometryTypes.Normal(-2/3,2/3,-1/3),
-     GeometryTypes.Normal(-2/3,2/3,-1/3),
-     GeometryTypes.Normal(2/3,-2/3,-1/3),
-     GeometryTypes.Normal(2/3,-2/3,-1/3),
-     GeometryTypes.Normal(2/3,-2/3,-1/3),
-     GeometryTypes.Normal(0.0,0.0,-1.0),
-     GeometryTypes.Normal(0.0,0.0,-1.0),
-     GeometryTypes.Normal(0.0,0.0,-1.0),
-     GeometryTypes.Normal(0.0,0.0,-1.0),
-     GeometryTypes.Normal(0.0,0.0,-1.0),
-     GeometryTypes.Normal(0.0,0.0,-1.0)]
+    d13 = round(1/3, 6)
+    d23 = round(2/3, 6)
+    md13 = round(-1/3, 6)
+    md23 = round(-2/3, 6)
+    d = Dict([ 1.0, 0.0, 0.0] => [Face([1.0,1.0,1.0], [1.0,-1.0,1.0], [1.0,1.0,2.0]),
+                                  Face([1.0,-1.0,1.0], [1.0,-1.0,2.0], [1.0,1.0,2.0]),
+                                  Face([1.0,1.0,1.0], [1.0,0.0,-1.0], [1.0,-1.0,1.0])],
+             [ 0.0, 1.0, 0.0] => [Face([-1.0,1.0,1.0], [1.0,1.0,1.0], [-1.0,1.0,2.0]),
+                                  Face([1.0,1.0,1.0], [1.0,1.0,2.0], [-1.0,1.0,2.0]),
+                                  Face([-1.0,1.0,1.0], [0.0,1.0,-1.0], [1.0,1.0,1.0])],
+             [ d23, d23,md13] => [Face([1.0,1.0,1.0], [0.0,1.0,-1.0], [1.0,0.0,-1.0])],
+             [ 0.0,-1.0, 0.0] => [Face([1.0,-1.0,1.0], [-1.0,-1.0,1.0], [1.0,-1.0,2.0]),
+                                  Face([-1.0,-1.0,1.0], [-1.0,-1.0,2.0], [1.0,-1.0,2.0]),
+                                  Face([1.0,-1.0,1.0], [0.0,-1.0,-1.0], [-1.0,-1.0,1.0])],
+             [-1.0, 0.0, 0.0] => [Face([-1.0,-1.0,1.0], [-1.0,1.0,1.0], [-1.0,-1.0,2.0]),
+                                  Face([-1.0,1.0,1.0], [-1.0,1.0,2.0], [-1.0,-1.0,2.0]),
+                                  Face([-1.0,-1.0,1.0], [-1.0,0.0,-1.0], [-1.0,1.0,1.0])],
+             [md23,md23,md13] => [Face([-1.0,-1.0,1.0], [0.0,-1.0,-1.0], [-1.0,0.0,-1.0])],
+             [md23, d23,md13] => [Face([-1.0,0.0,-1.0], [0.0,1.0,-1.0], [-1.0,1.0,1.0])],
+             [ d23,md23,md13] => [Face([1.0,0.0,-1.0], [0.0,-1.0,-1.0], [1.0,-1.0,1.0])],
+             [ 0.0, 0.0,-1.0] => [Face([0.0,-1.0,-1.0], [1.0,0.0,-1.0], [0.0,1.0,-1.0]),
+                                  Face([0.0,1.0,-1.0], [-1.0,0.0,-1.0], [0.0,-1.0,-1.0])])
+    test_decompose(p, d)
 end
 
 function decompose(lib::PolyhedraLibrary)
+    orthantdecompose(lib)
     cubedecompose(lib)
     largedecompose(lib)
 end
