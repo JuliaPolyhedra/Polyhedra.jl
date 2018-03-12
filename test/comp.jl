cd81e7e05ecc88dd795dd13528d460a5f74dea2c
Benoît Legat <benoit.legat@gmail.com>
2018-03-12 19:33:04 +0100

    Fix typo

--- test/comp.jl
+++ test/comp.jl
@@ -1,4 +1,4 @@
-@testset "Comparision" begin
+@testset "Comparison" begin
     @test SymPoint([1, 2]) ≈ SymPoint([-1, -2])
     @test !(SymPoint([1, 2]) ≈ SymPoint([-2, -1]))
     @test Polyhedra._lt(1, 1.5)
