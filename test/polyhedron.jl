310a00f62df261e4720b003151dc05305b1eb8f8
Benoît Legat <benoit.legat@gmail.com>
2018-03-12 21:33:35 +0100

    Add polyhedron tests

--- test/polyhedron.jl
+++ test/polyhedron.jl
@@ -7,4 +7,9 @@ end
     @test_throws ErrorException detecthlinearities!(p)
     @test_throws ErrorException eliminate(p, [1], FourierMotzkin())
     @test_throws ErrorException eliminate(p, [1], BlockElimination())
+    @test_throws ErrorException hrep(p)
+    @test_throws ErrorException vrep(p)
+    @test_throws ErrorException loadpolyhedron!(p, "a", "b")
+    @test_throws ErrorException loadpolyhedron!(p, "a", "ine")
+    @test_throws ErrorException loadpolyhedron!(p, "a", "ext")
 end
