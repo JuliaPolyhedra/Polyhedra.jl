b9e712c5e4356d78c4ea3961bfff2b967df30ac8
Benoît Legat <benoit.legat@gmail.com>
2018-09-20 11:48:11 +0200

    Add missing file

---
+++ src/dimension.jl
@@ -0,0 +1,44 @@
+import StaticArrays
+function similar_type(SAT::Type{<:StaticArrays.SVector},
+                      size::StaticArrays.Size, T::Type)
+    StaticArrays.similar_type(SAT, T, size)
+end
+
+# The dimension is Int for Vector, SparseVector and
+# StaticArrays.Size for StaticArrays.SVector
+# This allows similar_type to be compiled as only one method for Vector
+# and SparseVector and be type stable for StaticArrays.SVector
+"""
+    FullDim(p)::FullDim
+
+Similar to [`fulldim`](@ref) but used for type stability with the vector type.
+If the vector type is `StaticArrays.SVector` then it returns a
+`StaticArrays.Size`.
+"""
+const FullDim = Union{Int, StaticArrays.Size}
+
+FullDim(::Type{<:AbstractVector}) = 0 # Shouldn't hurt as it will not be used
+FullDim(v::AbstractVector) = length(v)
+FullDim(v::Union{StaticArrays.SVector{N}, Type{<:StaticArrays.SVector{N}}}) where N = StaticArrays.Size(v)
+
+"""
+    fulldim(rep::Rep)::Int
+
+Returns the dimension of the space in which polyhedron, representation,
+element or vector is defined. That is, a straight line in a 3D space has
+`fulldim` 3 even if its dimension is 1.
+"""
+fulldim(p) = fulldim(FullDim(p))
+
+function fulldim end
+
+fulldim(N::Int) = N
+fulldim(::StaticArrays.Size{N}) where N = N[1]
+
+neg_fulldim(N::Int) = -N
+# FIXME May need generated function to make it type stable
+neg_fulldim(::StaticArrays.Size{N}) where N = StaticArrays.Size{(-N[1],)}
+
+sum_fulldim(N1::Int, N2::Int) = N1 + N2
+# FIXME May need generated function to make it type stable
+sum_fulldim(::StaticArrays.Size{N1}, ::StaticArrays.Size{N2}) where {N1, N2} = StaticArrays.Size{(N1[1] + N2[1],)}
