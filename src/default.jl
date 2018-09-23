78b4587b368402ff78cf9de7ef31c03bb26ab4e5
Benoît Legat <benoit.legat@gmail.com>
2018-09-23 10:37:42 +0200

    Add examples for default_library

--- src/default.jl
+++ src/default.jl
@@ -25,6 +25,15 @@ end
     default_library(d::FullDim, ::Type{T}) where {T}
 
 Returns the default polyhedral library for `d`-dimensional polyhedron of coefficient type `T`.
+
+## Examples
+
+To obtain the default library for 2-dimensional polyhedra of eltype `Float64`,
+do `default_library(2, Float64)`.
+
+Given an `StaticArrays.SVector` `v`, to obtain a default library for points
+of the type of `v` in a type stable way, do
+`default_library(Polyhedra.FullDim(v), eltype(v))`.
 """
 function default_library end
 
