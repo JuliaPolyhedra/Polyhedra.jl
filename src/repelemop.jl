4442d350073150d10fb1b12f8647942b9927b04c
Benoît Legat <benoit.legat@gmail.com>
2019-05-09 18:20:52 +0200

    Fix escaping

--- src/repelemop.jl
+++ src/repelemop.jl
@@ -35,7 +35,7 @@ end
 Computes translation of the polyhedron `p` with the vector `v`.
 That is, computes
 ```math
-\{\, x + v \\mid x \\in p \,\}.
+\\{\\, x + v \\mid x \\in p \\,\\}.
 ```
 By default, if the H-representation, it simply translates every hyperplanes and halfspace,
 otherwise, it translates every points of the V-representation.
