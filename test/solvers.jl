18c5f2a655957ff518e1c9c9e6dc218ccd6f9679
Benoît Legat <benoit.legat@gmail.com>
2022-02-25 15:06:23 -0500

    Switch to HiGHS

--- test/solvers.jl
+++ test/solvers.jl
@@ -1,4 +1,3 @@
-import GLPK
-# Need `"presolve" => GLPK.ON` for `detect_new_linearities`, see
-# https://travis-ci.org/github/JuliaPolyhedra/Polyhedra.jl/jobs/691916637#L486
-lp_solver = optimizer_with_attributes(GLPK.Optimizer, "presolve" => GLPK.GLP_ON)
+using JuMP
+import HiGHS
+lp_solver = optimizer_with_attributes(HiGHS.Optimizer, MOI.Silent() => true)
