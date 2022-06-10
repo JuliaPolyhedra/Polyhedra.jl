c4d6100c18760563252fb83433d05325fd5d6c93
Benoît Legat <benoit.legat@gmail.com>
2022-06-10 10:48:08 +0200

    Mention removehredundancy in Fourier-Motzkin

--- src/projection.jl
+++ src/projection.jl
@@ -8,7 +8,11 @@ struct DefaultElimination <: EliminationAlgorithm end
 """
     FourierMotzkin
 
-Computation of the projection by computing the H-representation and applying the Fourier-Motzkin elimination algorithm to it.
+Computation of the projection by computing the H-representation and applying the
+Fourier-Motzkin elimination algorithm to it.
+
+The resulting polyhedron may contain duplicates and/or redundancy. These can be
+removed with [`removehredundancy!`](@ref).
 """
 struct FourierMotzkin <: EliminationAlgorithm end
 
