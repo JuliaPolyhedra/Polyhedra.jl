ef7206dd474ac9a5d7d60aa995b5205746271a38
Benoît Legat <benoit.legat@gmail.com>
2026-03-24 09:45:12 +0100

    Explicitly prefix simplify in tests (#359)

--- test/elements.jl
+++ test/elements.jl
@@ -13,24 +13,24 @@ using Polyhedra
         @test r != Line([1, -5, -15])
     end
     @testset "Simplify" begin
-        r = @inferred simplify(Ray([0, 0]))
+        r = @inferred Polyhedra.simplify(Ray([0, 0]))
         @test r isa Ray{Int}
         @test coord(r) == [0, 0]
-        p = @inferred simplify([6, 4])
+        p = @inferred Polyhedra.simplify([6, 4])
         @test p isa Vector{Int}
         @test p == [6, 4]
-        l = @inferred simplify(Line([6, 4]))
+        l = @inferred Polyhedra.simplify(Line([6, 4]))
         @test l isa Line{Int}
         @test coord(l) == [3, 2]
-        hp = @inferred simplify(HyperPlane([6, 4], 2))
+        hp = @inferred Polyhedra.simplify(HyperPlane([6, 4], 2))
         @test hp isa HyperPlane{Int}
         @test hp.a == [3, 2]
         @test hp.β == 1
-        hs = @inferred simplify(HalfSpace([0, 0], 0))
+        hs = @inferred Polyhedra.simplify(HalfSpace([0, 0], 0))
         @test hs isa HalfSpace{Int}
         @test hs.a == [0, 0]
         @test hs.β == 0
-        hpr = @inferred simplify(HyperPlane([0//1, 0//1], 0//1))
+        hpr = @inferred Polyhedra.simplify(HyperPlane([0//1, 0//1], 0//1))
         @test hpr isa HyperPlane{Rational{Int}}
         @test hpr.a == [0, 0]
         @test hpr.β == 0
