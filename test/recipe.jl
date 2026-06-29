4aa9f8353f7429ece33ee3a4453aa009dc054718
Rainer Rodrigues <45894111+rainerrodrigues@users.noreply.github.com>
2026-06-29 13:51:04 +0530

    Fix Plots.jl recipe to forward label and series_annotations (#356) (#372)
    * Fix Plots.jl recipe to forward label and series_annotations (#356)
    
    * Add test for Plots.jl label and series_annotations forwarding
    
    * Refactor test to use RecipesBase.apply_recipe instead of Plots

--- test/recipe.jl
+++ test/recipe.jl
@@ -31,4 +31,29 @@ function recipetest(lib::Polyhedra.Library)
         p = polyhedron(h, lib)
         @test_throws err RecipesBase.apply_recipe(Dict{Symbol, Any}(), p)
     end
+
+    @testset "Plots.jl attribute forwarding" begin
+        # Creating a simple 2D polyhedron
+        v = vrep([0 0; 1 0; 0 1; 1 1])
+        p = polyhedron(v)
+
+        # Simulating the user providing custom kwargs to plot()
+        # This is exactly what Plots.jl passes into the macro internally
+        attrs_with_label = Dict{Symbol, Any}(:label => "Custom Label", :series_annotations => ["1", "2", "3", "4"])
+    
+        # Calling the recipe directly without needing Plots.jl
+        # This returns an array of RecipeData objects
+        res_with_label = RecipesBase.apply_recipe(attrs_with_label, p)
+        series_attrs = res_with_label[1].plotattributes
+
+        # Verifying custom label prevented `legend --> false` and annotations were kept
+        @test !haskey(series_attrs, :legend) || series_attrs[:legend] !== false
+        @test haskey(series_attrs, :series_annotations)
+        @test series_attrs[:series_annotations] == ["1", "2", "3", "4"]
+
+        # Testing that if the user DOES NOT provide a label, legend defaults to false
+        attrs_no_label = Dict{Symbol, Any}()
+        res_no_label = RecipesBase.apply_recipe(attrs_no_label, p)
+        @test res_no_label[1].plotattributes[:legend] == false
+    end
 end
